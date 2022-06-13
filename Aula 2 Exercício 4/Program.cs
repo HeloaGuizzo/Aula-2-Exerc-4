6ba169f99d3ce569ada8849d062e4af6fe10d08d
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 18:40:52 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 2 Exercício 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- CÁLCULO DA IDADE EM MESES E DIAS -----");
+
+            Console.WriteLine("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            int meses = idade * 12;
+            int dias = meses * 30;
+
+            Console.WriteLine(" A sua idade em meses totaliza: " + (meses) + " " + "meses.");
+            Console.WriteLine(" Já em dias totaliza :" + (dias) + " " + "dias.");
+        }
+    }
+}
